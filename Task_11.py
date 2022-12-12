8d7ff639143c4861b41d7809eaa88bdc17db8c13
Mephod1y <73156817+Mephod1y@users.noreply.github.com>
2022-12-12 16:10:05 +0200

    Add files via upload

---
+++ Task_11.py
@@ -0,0 +1,120 @@
+from Task_11_classes import AddressBook, Record
+
+contacts_dict = AddressBook()
+
+def input_error(function):
+    def wrapper(*args, **kwargs):
+        try:
+            return function(*args, **kwargs)
+        except KeyError:
+            return 'Wrong name'
+        except ValueError as exception:
+            return exception.args[0]
+        except IndexError:
+            return 'Enter name and number'
+        except TypeError:
+            return 'Wrong command'
+    return wrapper
+
+@input_error
+def hello():
+    return 'How can I help you?'
+
+@input_error
+def exit_func():
+    return "Good bye"
+
+@input_error
+def name_phone(data):  # функція перевіряє на коректність введення ім'я телефону
+    new_data = data.strip().split(" ")
+    name = new_data[0]
+    phone = new_data[1]
+    if name.isnumeric():
+        raise ValueError('Wrong name')
+    if not phone.isnumeric():
+        raise ValueError('Wrong phone')
+    return name, phone
+
+@input_error
+def add_name_phone(data):  # функція додає ведене ім'я телефон до екземляру класу Record
+    name, phones = name_phone(data)
+    if name in contacts_dict:
+        raise ValueError('This contact already exist.')
+    record = Record(name)
+    record.put_phone_list(phones)
+    contacts_dict.add_record(record)
+    return f'You added new contact: {name} with this {phones}'
+
+@input_error
+def show_all():  # функція повертає всі записи в словнику AddressBook
+    contacts = "All data in AddressBook:"
+    for name in contacts_dict:
+        contacts += f'\n{name}: '
+        for phone in contacts_dict[name].phones:
+            contacts += f"{phone.value}"
+    return contacts
+
+@input_error
+def change_phone(user_input):  # функція змінює телефон по ключу в словнику AddressBook
+    user_input = user_input.split()
+    name = user_input[0]
+    current_phone = user_input[1]
+    new_phone = user_input[2]
+    return contacts_dict[name].change(current_phone, new_phone)
+
+@input_error
+def show_phone(user_input): # функція повертає телефон по ключу в словнику AddressBook
+    phones = ""
+    user_input = user_input.split()
+    name = user_input[0]
+    for phone in (contacts_dict[name]).phones:
+        phones += f"{phone.value} "
+    return phones
+
+@input_error
+def add_birth(user_input):
+    user_input = user_input.split()
+    name = user_input[0]
+    birthday = user_input[1]
+    return contacts_dict[name].add_birthday(birthday)
+
+functions = {
+    'hello': hello,
+    'add': add_name_phone,
+    'change': change_phone,
+    'phone': show_phone,
+    'show all': show_all,
+    'exit': exit_func,
+    'close': exit_func,
+    'good bye': exit_func,
+    'birth': add_birth
+}
+
+def parser_input(user_input):
+    new_input = user_input
+    data = ''
+    for key in functions:
+        if user_input.strip().lower().startswith(key):
+            new_input = key
+            data = user_input[len(new_input):]
+            break
+    if data:
+        return reaction_func(new_input)(data)
+    return reaction_func(new_input)()
+
+def reaction_func(reaction):
+    return functions.get(reaction, break_func)
+
+def break_func():
+    return 'Wrong command'
+
+def main():
+    while True:
+        user_input = input('Enter command: ')
+        result = parser_input(user_input)
+        print(result)
+        if result == 'Good bye':
+            break
+
+if __name__ == "__main__":
+    main()
---
+++ Task_11_classes.py
@@ -0,0 +1,80 @@
+from collections import UserDict
+from datetime import datetime
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def show_all(self):
+        return self.data
+
+    def get_data(self, name):
+        return self.data.get(name)
+
+class Field():
+    def __init__(self, value):
+        self.value = value
+
+class Name(Field):
+    def __init__(self, value):
+        self.value = value
+
+class Phone(Field):
+    def __init__(self, value):
+        self.value = value
+
+class Birthday(Field):
+    def __init__(self, value):
+        self.value = value
+
+class Record():
+    def __init__(self, name, *phones):
+        self.name = Name(name)
+        self.phones = []
+        if phones:
+            for phone in phones:
+                self.put_phone_list(phone)
+        self.birthday = ''
+
+    def put_phone_list(self, phone_new):
+        phone_new = Phone(phone_new).value
+        for phone in self.phones:
+            if phone_new == phone.value:
+                print(f"{phone_new} already recorded for {self.name.value}")
+        self.phones.append(Phone(phone_new))
+
+    def change(self, phone_old, phone_new):
+
+        phone_old = Phone(phone_old).value
+        phone_new = Phone(phone_new).value
+        match = False
+
+        for phone in self.phones:
+
+            if phone.value == phone_new:
+                return f"{phone_new} already recorded for {self.name.value}"
+
+            if phone.value == phone_old:
+                match = True
+
+        if not match:
+            return f"{phone_old} exist in the contact {self.name.value}"
+
+        for index, phone in enumerate(self.phones):
+            if phone.value == phone_old:
+                self.phones.remove(phone)
+                self.phones.insert(index, Phone(phone_new))
+                return f"{phone_old} changed to {phone_new}"
+    
+    def add_birthday(self, birthday):
+        self.birthday = Birthday(birthday)
+        return f"{birthday} added to {self.name}"
+
+    def days_to_birthday(self):
+        # birthday = self.birthday
+        today = datetime.today()
+        bday = datetime(2022,int(self.birthday.split('.')[1]),int(self.birthday.split('.')[2]))
+        bday1 = datetime(2023,int(self.birthday.split('.')[1]),int(self.birthday.split('.')[2]))
+        timediff = (bday - today).days + 1 if (today - bday).days < 0 else (bday1 - today).days + 1
+        return f'{timediff} days till birthday left!'
+            
